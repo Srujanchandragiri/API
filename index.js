3707da8d16664adacf50c154936537c5ec64fee6
Srujanchandragiri <145650122+Srujanchandragiri@users.noreply.github.com>
2023-09-25 11:18:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const BrandName =require('./model');
 
-const app =express();
+const app = express();
 
 app.use(express.json())
 
 mongoose.connect('mongodb+srv://Srujan:Srujanc12@atlascluster.gjrriix.mongodb.net/?retryWrites=true&w=majority',{
-    useUnifiedTopology:true,
+    useUnifiedTopology: true,
     useNewUrlParser:true
-    }).then(
+}).then(
     ()=>console.log('DB connected')
 ).catch(err => console.log(err))
 
-app.post('/addbrands',async(req,res) => {
+
  
- const{brandname} = req.body;
+app.post('/addbrands',async (req,res) =>{
+    
     try{
-        const newData = new BrandName({brandname});
-        await newData.save();
-        return res.send( BrandName.find())
+        // const newData = new BrandName({brandname})
+        // await newData.save();
+        // return res.json( BrandName.find())
+        console.log("response",req.body);
 
- }
- catch(err){
-    console.log(err.message);
- }
-})
+        return res.json("Done!");
 
+    }
+    catch(err){
+        console.log(err.message);
+        return res.json("Error!,Please try again");
+    }
+}) 
 
-
-app.listen(4040,()=>console.log('Server running... '))
+app.listen(6050,()=>console.log('Server running... '))
